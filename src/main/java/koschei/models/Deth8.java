a31b523c9046fd94d2a50382ee680749b97c43b5
Efrem Shpakov <shpakovev95@gmail.com>
2023-03-08 19:15:27 +0300

    ev.shpakov PP_2_1_4 rev.1

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,13 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        String fairyTaleFinal = " Вот и сказочке конец!!!";
+        return fairyTaleFinal;
+    }
 }
--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -4,9 +4,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    private Egg6 egg6;
+
+
+    public Duck5(Egg6 egg6) {
+        System.out.println("Создаем яйцо");
+        this.egg6 = egg6;
+    }
+
+
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg6.toString();
     }
 }
